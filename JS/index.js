73e1572503aa72c83334ffebaad5431fe1173f8d
Leamourad <139025477+Leamourad@users.noreply.github.com>
2023-12-14 21:19:06 -0600

    Week20
    Added files

---
+++ JS/index.js
@@ -0,0 +1,37 @@
+/*
+const button01 = document.querySelector("button01")
+button01.addEventListener ("mouseover"), () => {
+button01.style.bordercolor= "1px solid black"; 
+};
+   
+button01.addEventListener ("mouseout"), () => {
+
+
+box.style.border = "#8C9CB6";
+button01.style.bordercolor= "1px solid red";
+}
+button01.addEventListener ("mouseover"); 
+button01.addEventListener ("mouseout");
+*/
+
+/*
+$(".Img03").hover (
+    function() {$(this).animate({width:"438px", height:"546px"} )},
+    function() {$(this).animate({width:"338px", height:"446px"} )}
+);
+*/
+/*
+$(document).ready (function () {
+    $(".Img03") .mouseover(function() {
+        $(this).stop().animate({"width":"438px", "height":"546px"},500,"swing");
+    }).mouseout(function() {
+        $(this).stop().animate({"width":"338px", "height":"446px"},500,"swing");
+    });
+    });
+    */
+   /*
+   const testing=document.getElementsByClassName ("Img03");
+   testing.addEventListener(mouseover,()=>{
+    testing.style.transform=scale(3)
+   });
+   */
